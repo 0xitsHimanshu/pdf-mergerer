fcf3a3634f40e897c4c7e43e399e1d25c28ac8f6
Himanshu <kyadav.9643@gmail.com>
2023-07-28 12:53:20 +0530

    updated server.js
    Solved the bug by which all the pdfs are merging together and set the fs system to delete the uploaded files from the server ASAP the pdfs are merged

--- server.js
+++ server.js
@@ -3,29 +3,35 @@ const path = require('path')
 const app = express()
 const multer = require('multer')
 const port = 3000
-const upload = multer({dest: 'uploads/'})
-const {mergePDFs} = require("./merger")
+const upload = multer({ dest: 'uploads/' })
+const { mergePDFs } = require("./merger")
+const fs = require('fs')
 
-app.use('/static',express.static('public'))
+app.use('/static', express.static('public'))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname,'template/index.html'))
+  res.sendFile(path.join(__dirname, 'template/index.html'))
 })
 
-app.post('/merge', upload.array('pdfs',2),async (req,res)=>{
-    const uploadedFiles = req.files;
-    console.log(uploadedFiles);
-    
-    if (!uploadedFiles || uploadedFiles.length === 0) {
-        return res.status(400).json({message:'No files were uploaded. '});
-    }
-    try {
-        const mergeFileName = await mergePDFs(uploadedFiles[0].path, uploadedFiles[1].path)
-        res.redirect(`http://localhost:${port}/static/${mergeFileName}.pdf`)
+app.post('/merge', upload.array('pdfs', 2), async (req, res) => {
+  try {
+    console.log(req.file)
+    let d = await mergePDFs(path.join(__dirname, req.files[0].path), path.join(__dirname, req.files[1].path))
+    res.redirect(`http://localhost:${port}/static/${d}.pdf`)
 
-    } catch (error) {
-        console.error('Error while merging the PDF fils:',error);
-    }
+    req.files.forEach(file => {
+      fs.unlink(file.path, (err) => {
+        if (err)
+          console.error('Error  deleting file:', file.path);
+        else
+          console.log('File Deleted successfully:', file.path)
+      });
+    });
+
+  } catch (err) {
+    console.error('Error merging PDFs:', err)
+    res.status(500).send('Error mergin PDFs;')
+  }
 })
 
 app.listen(port, () => {
